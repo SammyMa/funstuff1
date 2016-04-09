afa0171ae0c71767fe34dc85b318a75e589dea32
Jing Ma <jim027@ieng6-201.ucsd.edu>
2016-04-09 13:03:35 -0700

    done with first three parts without optimization

--- assignment1_p2.py
+++ assignment1_p2.py
@@ -14,14 +14,14 @@ def isprime(n):
            return False
     return True
 
-def inlist(toBeCheck,position):
+def inlist(toBeCheck, position):
 	try:
-		result = toBeCheck.index(position)         
+		result = toBeCheck.index(position)
 	except ValueError:
 		return False
 	else:
-	 	return True
-	
+	  	return True
+
 def getPossibleActions(currentPrime):
 	listOfPrimes = []
 	temp = currentPrime
@@ -35,35 +35,43 @@ def getPossibleActions(currentPrime):
 			for loop in range (1,10):
 				temp = temp + (10 ** index)
 				if isprime(temp) and inlist(listOfPrimes, temp) == False:
+					print("prime is %d" %temp)
 					listOfPrimes.append(temp)    
 		else: 
 			tempindex = index + 1
 			temp = (temp / (10 ** tempindex)) * (10 ** tempindex) + (temp % (10 ** index))
+			print ("HERE IS: %d" %temp)
 			origin = temp
 			for loop in range(0, 10):
+				print(loop)
 				temp = origin + loop * (10 ** index)
+				print("here is: %d" %temp)
 				if isprime(temp) and inlist(listOfPrimes, temp) == False:
+					print("prime is %d" %temp)
 					listOfPrimes.append(temp)
 		temp = currentPrime
-	#listOfPrimes.reverse()
 	return listOfPrimes
 
 def getPath(startingPrime, finalPrime):
 	startingPrime = int(startingPrime)
 	finalPrime = int(finalPrime)
-	queue = [(startingPrime, [startingPrime])]
+	stack = [(startingPrime,[startingPrime])]
 	visited = []
 	added = []
-	while queue:
-		(key,path) = queue.pop(0)
+	while stack:
+		(key,path) = stack.pop()
 		visited.append(key)
 		children = getPossibleActions(key)
 		for elem in children:
-			if elem == finalPrime:
+			if elem == finalPrime and len(path) < 6:
 				return ' '.join(map(str,path + [elem]))
+			elif elem != finalPrime and len(path) >= 6:
+				if inlist(visited, key) == True:
+					visited.remove(key)
+				break
 			else:
 				if inlist(visited, elem) == False and inlist(added, elem) == False:
-					queue.append((elem,path+[elem]))
+					stack.append((elem,path+[elem]))
 					added.append(elem)
 
 	return "UNSOLVABLE"
--- assignment1_p3.py
+++ assignment1_p3.py
@@ -14,14 +14,14 @@ def isprime(n):
            return False
     return True
 
-def inlist(toBeCheck,position):
+def inlist(toBeCheck, position):
 	try:
-		result = toBeCheck.index(position)         
+		result = toBeCheck.index(position)
 	except ValueError:
 		return False
 	else:
-	 	return True
-	
+	  	return True
+
 def getPossibleActions(currentPrime):
 	listOfPrimes = []
 	temp = currentPrime
@@ -34,37 +34,44 @@ def getPossibleActions(currentPrime):
 			temp = temp % (10 ** index)
 			for loop in range (1,10):
 				temp = temp + (10 ** index)
-				if isprime(temp) and inlist(listOfPrimes, temp) == False:
+				if inlist(listOfPrimes, temp) == False and isprime(temp):
 					listOfPrimes.append(temp)    
 		else: 
 			tempindex = index + 1
 			temp = (temp / (10 ** tempindex)) * (10 ** tempindex) + (temp % (10 ** index))
+			print ("HERE IS: %d" %temp)
 			origin = temp
 			for loop in range(0, 10):
+				print(loop)
 				temp = origin + loop * (10 ** index)
-				if isprime(temp) and inlist(listOfPrimes, temp) == False:
+				print("here is: %d" %temp)
+				if inlist(listOfPrimes, temp) == False and isprime(temp):
 					listOfPrimes.append(temp)
 		temp = currentPrime
-	#listOfPrimes.reverse()
 	return listOfPrimes
 
 def getPath(startingPrime, finalPrime):
-	startingPrime = int(startingPrime)
-	finalPrime = int(finalPrime)
-	queue = [(startingPrime, [startingPrime])]
-	visited = []
-	added = []
-	while queue:
-		(key,path) = queue.pop(0)
-		visited.append(key)
-		children = getPossibleActions(key)
-		for elem in children:
-			if elem == finalPrime:
-				return ' '.join(map(str,path + [elem]))
-			else:
-				if inlist(visited, elem) == False and inlist(added, elem) == False:
-					queue.append((elem,path+[elem]))
-					added.append(elem)
+	for num in range(0, 9):
+		startingPrime = int(startingPrime)
+		finalPrime = int(finalPrime)
+		stack = [(startingPrime,[startingPrime])]
+		visited = []
+		added = []
+		while stack:
+			(key,path) = stack.pop()
+			visited.append(key)
+			children = getPossibleActions(key)
+			for elem in children:
+				if elem == finalPrime and len(path) < num:
+					return ' '.join(map(str,path + [elem]))
+				elif elem != finalPrime and len(path) >= num:
+					if inlist(visited, key) == True:
+						visited.remove(key)
+					break
+				else:
+					if inlist(visited, elem) == False:
+						stack.append((elem,path+[elem]))
+						added.append(elem)
 
 	return "UNSOLVABLE"
 
